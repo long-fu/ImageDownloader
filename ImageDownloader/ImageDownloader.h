4390b3c71ea6a4449a61e040ce3ad0b8d293dc99
haoshuai <haoshuai9192@gmail.com>
2020-03-14 02:54:28 +0800

    Initial Commit

---
+++ ImageDownloader/ImageDownloader.h
@@ -0,0 +1,19 @@
+//
+//  ImageDownloader.h
+//  ImageDownloader
+//
+//  Created by haoshuai on 2020/3/14.
+//  Copyright © 2020 haoshuai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageDownloader.
+FOUNDATION_EXPORT double ImageDownloaderVersionNumber;
+
+//! Project version string for ImageDownloader.
+FOUNDATION_EXPORT const unsigned char ImageDownloaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageDownloader/PublicHeader.h>
+
+
