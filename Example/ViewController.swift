e999d455bb10e4f7888b54f96242a03037a4b3c5
haoshuai <haoshuai9192@gmail.com>
2020-03-19 02:02:10 +0800

    动画部分可以参考

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -15,6 +15,51 @@ struct ImageLoader {
 
 }
 
+class AN: UICollectionViewLayoutAttributes {
+    
+}
+
+class AnimationCollectionViewLayout: UICollectionViewFlowLayout {
+    
+//    override func initialLayoutAttributesForAppearingItem(at itemIndexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+//
+//    }
+    
+    let contentSize: CGSize
+    init(contentSize: CGSize) {
+        self.contentSize = contentSize
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+//    override var collectionViewContentSize: CGSize {
+//        return self.contentSize
+//    }
+    
+    override func layoutAttributesForInteractivelyMovingItem(at indexPath: IndexPath, withTargetPosition position: CGPoint) -> UICollectionViewLayoutAttributes {
+        let attr1 = super.layoutAttributesForInteractivelyMovingItem(at: indexPath, withTargetPosition: position)
+        guard let attr = self.layoutAttributesForItem(at: indexPath) else {
+            fatalError()
+        }
+        attr.transform =  CGAffineTransform.init(scaleX: 0.2, y: 0.2).rotated(by: .pi)
+
+        attr.center = CGPoint(x: self.collectionView!.bounds.midX, y: self.collectionView!.bounds.midY)
+        debugPrint("动画部分")
+        return attr;
+    }
+    
+    override func initialLayoutAttributesForAppearingItem(at itemIndexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        debugPrint("增加数据动画")
+        return nil
+    }
+    
+    
+}
+
 class ViewController: UIViewController {
 
     
@@ -22,25 +67,73 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var reloadButton: UIButton!
     
+    private var slayout: AnimationCollectionViewLayout?
+    
+    private var blayout: AnimationCollectionViewLayout?
+    
+    private var isSamll = true
+    
     var dataSource: [URL] = []
+    
+    var allDataSource: [URL] = []
+    
+    var index = 0
+    
+    private let pages = 100
+    
+    private var loading: Bool = false
+    
+    private func loadNewData() {
+        
+        
+        for i in 0 ..< pages {
+            if index < allDataSource.count {
+                let item = allDataSource[i]
+                dataSource.append(item)
+                index += 1
+            } else {
+                break
+            }
+        }
+        self.collectionView.reloadData()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urls = ImageLoader.sampleImageURLs.map{URL.init(string: $0)!}
-        
-        self.dataSource = urls
-        
+        allDataSource = ImageLoader.sampleImageURLs.map{URL.init(string: $0)!}
+
+        for i in 0 ..< pages {
+            if index < allDataSource.count {
+                let item = allDataSource[i]
+                dataSource.append(item)
+                index += 1
+            } else {
+                break
+            }
+        }
+        debugPrint("加载的",dataSource.count)
         reloadButton.addTarget(self, action: #selector(self.buttonAction(_:)), for: UIControl.Event.touchUpInside)
-        
         collectionView.delegate = self
         collectionView.dataSource = self
-    
-        let layout = UICollectionViewFlowLayout()
+        
+//        collectionView.setconview
+        let contentSize = UIScreen.main.bounds.inset(by: view.safeAreaInsets)
+        let layout = AnimationCollectionViewLayout(contentSize: contentSize.size)
         layout.itemSize = CGSize(width: 100, height: 100)
         layout.minimumInteritemSpacing = 2
         layout.minimumLineSpacing = 2
+        self.slayout = layout
+        
+        let layout2 = AnimationCollectionViewLayout(contentSize: contentSize.size)
+        layout2.itemSize = CGSize(width: 130, height: 130)
+        layout2.minimumInteritemSpacing = 2
+        layout2.minimumLineSpacing = 2
+        self.blayout = layout2
+        
+//        layout.collectionViewContentSize =
         collectionView.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: "ImageCollectionViewCell")
+//        self.collectionView.collectionViewLayout = layout
         collectionView.setCollectionViewLayout(layout, animated: false, completion: nil)
         
     }
@@ -48,13 +141,22 @@ class ViewController: UIViewController {
     
     @objc
     func buttonAction(_ sender: Any?) {
-        
+        if isSamll {
+            self.collectionView.setCollectionViewLayout(blayout!, animated: true)
+        } else {
+            self.collectionView.setCollectionViewLayout(slayout!, animated: true)
+        }
+        isSamll = !isSamll
     }
     
     
 }
 
 extension ViewController: UICollectionViewDataSource {
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return dataSource.count
@@ -144,3 +246,20 @@ extension ViewController: UICollectionViewDataSourcePrefetching {
     }
 }
 
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+}
+
+
+extension ViewController: UIScrollViewDelegate {
+        func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+            let distance = scrollView.contentSize.height - (targetContentOffset.pointee.y + scrollView.bounds.height)
+            debugPrint(distance,loading)
+            if !loading && distance < 200 {
+                loading = true
+                self.loadNewData()
+                loading = false
+            } // if
+            
+        }
+}
